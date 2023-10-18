94ec58d682e56720ef7e4da398aabe48f1f3eb4a
jacobL <ukljm.2005@gmail.com>
2023-10-18 17:26:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def index():
     cur = conn.cursor()
     cur.execute('select * from aism_accounts')
     for r in cur :
-        n = r[1]+os.getenv("gg")
+        n = r[1]+os.environ['gg']   #os.getenv("gg")
     #print(r)
     
     print('Request for index page received : ',n)
